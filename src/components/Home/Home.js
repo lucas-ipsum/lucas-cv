7aa47ccfb242997fb66d7fbe2e02d06b5ea7e197
s0572357 <s0572357@htw-berlin.de>
2022-12-15 16:39:15 +0100

    Init MUI

---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Box} from '@mui/material';
+
+function Home(props) {
+
+
+// Render Function 
+return (
+<Box>
+    <h1>Home</h1>
+
+</Box>
+
+)
+}
+
+export default Home; 
