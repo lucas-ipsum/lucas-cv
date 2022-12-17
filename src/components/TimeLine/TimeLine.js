4eea49e329a62607d4a85ff8fc1138dab2fe90a9
s0572357 <s0572357@htw-berlin.de>
2022-12-17 17:31:14 +0100

    endDate added in TimeLine component

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -5,35 +5,39 @@ import style from '../sytle/style';
 import TimeLineItem from '../TimeLineItem/TimeLineItem';
 import data from '../../data/cvTimeline';
 
+
 function TimeLine(props) {
 var timeLineItems = []; 
-// Sort Date 
 
-// Get TimeLine Data 
-for (let i = 0; i < data.length; i++) {
-    // Sort Date 
-    data[i].beginDate = new Date(data[i].beginDate);        // Set Type Date 
-
-    // Call Component
-}
-console.log(data)
-data.sort((a, b) => a.beginDate - b.beginDate);
-
-// Component
-for (let i = 0; i < data.length; i++) {
-    // data[i].beginDate = data[i].beginDate
+// Helper Function 
+// Date to String Format 
+const formatDate = (date) => {
     // Date Format
-    var dateYear = data[i].beginDate.getFullYear().toString();
-    console.log( data[i].beginDate.getMonth())
-    var dateMonth = data[i].beginDate.getMonth() + 1;
+    var dateYear = date.getFullYear().toString();
+    var dateMonth = date.getMonth() + 1;
     dateMonth.toString();
     if (dateMonth < 10 ) {
         dateMonth = '0' + dateMonth
     }
     var date = dateMonth + '/' + dateYear
-    timeLineItems.push(<TimeLineItem title={data[i].title} beginDate={date} category={data[i].category} />)    
+    return date; 
 }
-console.log(data)
+
+// Parse date to Date Format
+for (let i = 0; i < data.length; i++) {
+    data[i].beginDate = new Date(data[i].beginDate);        // Set Type Date 
+    data[i].endDate = new Date(data[i].endDate);        // Set Type Date 
+}
+// Sort Data List 
+data.sort((a, b) => a.beginDate - b.beginDate);
+
+// Call TimeLineItem Components
+for (let i = data.length - 1; i > 0; i--) {
+    var beginDate = formatDate(data[i].beginDate)
+    var endDate = formatDate(data[i].endDate)
+    // Component Array
+    timeLineItems.push(<TimeLineItem title={data[i].title} endDate={endDate} beginDate={beginDate} category={data[i].category} />)    
+}
 
 // Render Function 
 return (
