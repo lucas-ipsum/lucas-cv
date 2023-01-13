04d4f62557ad7978952a09e71330132e878a0875
s0572357 <s0572357@htw-berlin.de>
2023-01-13 12:13:22 +0100

    Init test adding action btn to conatct section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,11 @@
 import React from 'react'
 // MUI 
-import { Box, IconButton } from '@mui/material';
+import { Box, IconButton, SpeedDial, SpeedDialAction } from '@mui/material';
+// Icons 
 import MailIcon from '@mui/icons-material/Mail';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import PhoneIcon from '@mui/icons-material/Phone';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 function Contact (props) {
 
@@ -17,11 +19,44 @@ const linkedinButtonClicked = (e) => {
 const phoneButtonClicked = (e) => {
     window.open('tel:+4915127063731', '_self')
 }
-// 
+
+const copyToClipboard = (text) => {
+  navigator.clipboard.writeText('test');
+}
+const hover = () => {
+    console.log('hober')
+}
+
+// Actions Speed Dial Btn 
+const actions = [
+    { icon: <ContentCopyIcon />, name: 'Copy' },
+]
+
+// Icons 
+const mailIcon =       
+    <IconButton onClick={mailButtonClicked} aria-label="email" size="large">
+        <MailIcon color='primary' sx={{fontSize: '45px'}} />
+    </IconButton>
 
 // Render Function 
 return (
     <Box>
+            <SpeedDial 
+                // icon={mailIcon}
+                icon={ <MailIcon color='primary' sx={{fontSize: '45px'}} />}
+                ariaLabel="SpeedDial basic example"
+                sx={{'& .MuiFab-primary': { backgroundColor: '#4DA1C2' }}}
+            >
+                { actions.map((action) => (
+                <SpeedDialAction
+                    key={action.name}
+                    icon={action.icon}
+                    tooltipTitle={action.name}
+                    onClick={copyToClipboard}
+                    onHover={hover}
+                /> 
+                ))}
+            </SpeedDial>
         <IconButton onClick={mailButtonClicked} aria-label="email" size="large">
             <MailIcon color='primary' sx={{fontSize: '45px'}} />
         </IconButton>
